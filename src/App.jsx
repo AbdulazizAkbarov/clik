419c021f9ae84acc5a5b35b99103b679b0798036
Abdulaziz <abdulazizakbarov065@gmail.com>
2025-01-15 16:28:40 +0500

    FInished

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,34 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [number, setNumber] = useState(0);
   const [move, setMove] = useState(0);
-  const[counter, setCounter] =useState(0)
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const nomi = setInterval(() => {
       setNumber(Math.floor(Math.random(number) * 100));
       setMove(Math.floor(Math.random(move) * 1000));
     }, 1000);
+
+    setTimeout(()=>{
+      clearInterval(nomi)
+
+      alert("Vaqt Tugadi")
+
+
+    },20000)
   }, []);
+
+  
 
   return (
     <>
-
-    <h2 style={{
-      fontFamily:"sans-serif"
-    }}>Counter : {counter}</h2>
-
+      <h2
+        style={{
+          fontFamily: "sans-serif",
+        }}
+      >
+        Counter : {counter}
+      </h2>
 
       <div
         style={{
@@ -35,9 +47,8 @@ function App() {
             background: "darkblue",
             borderRadius: "50%",
           }}
-
-          onClick={()=>{
-            setCounter(counter + 1)
+          onClick={() => {
+            setCounter(counter + 1);
           }}
         ></div>
       </div>
